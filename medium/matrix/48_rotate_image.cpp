c6aa2da700686491c34647c17a365fdbbfdc47a8
Smit0401 <smit040106@gmail.com>
2026-02-10 23:31:12 +0530

    Create 48_rotate_image.cpp

---
+++ medium/matrix/48_rotate_image.cpp
@@ -0,0 +1,26 @@
+// 48. Rotate Image
+// Difficulty: Medium
+// Pattern: Matrix Transpose + Reverse
+// Time: O(n^2)
+// Space: O(1)
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n=matrix.size();
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                  swap(matrix[i][j],matrix[j][i]); 
+            }
+        }
+        for(int i=0;i<n;i++){
+            int j=0;
+            int k=n-1;
+            while(j<=k){
+                swap(matrix[i][j],matrix[i][k]); 
+                j++;
+                k--;
+            }
+        }
+    }
+};
